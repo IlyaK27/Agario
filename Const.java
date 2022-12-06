c4962949c3c6aa8a2dbae22a259186546cb1cb87
IlyaK27 <107894670+IlyaK27@users.noreply.github.com>
2022-12-06 15:22:44 -0500

    Add files via upload

---
+++ Const.java
@@ -0,0 +1,29 @@
+import java.awt.Color;
+import java.awt.Font;
+
+public class Const{
+    public static final int WIDTH = 1000;
+    public static final int HEIGHT = 800;
+    public static final int STARTING_X = WIDTH/2;
+    public static final int STARTING_Y = HEIGHT/2;
+    public static final int MAX_HORIZONTAL_SPEED = 20;
+    public static final int MAX_VERTICAL_SPEED = 20;
+    public static final int GROWTH_RATE = 40;
+    public static final Color FONT_COLOR = Color.BLACK; 
+    public static final Font BALL_FONT = new Font("Arial", Font.PLAIN, 20);
+
+    private Const(){}
+}
+ublic class Const{
+    public static final int WIDTH = 1000;
+    public static final int HEIGHT = 800;
+    public static final int STARTING_X = WIDTH/2;
+    public static final int STARTING_Y = HEIGHT/2;
+    public static final int MAX_HORIZONTAL_SPEED = 20;
+    public static final int MAX_VERTICAL_SPEED = 20;
+    public static final int GROWTH_RATE = 40;
+    public static final Color FONT_COLOR = Color.BLACK; 
+    public static final Font BALL_FONT = new Font("Arial", Font.PLAIN, 20);
+
+    private Const(){}
+}
